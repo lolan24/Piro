1d0fe3f158217f9e839373598badcc71c2aaf252
lolan24 <asianetbot@gmail.com>
2024-12-05 14:23:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ SUPPORT_CHAT_ID = -1001968355911
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lolan4:lolan4@cluster0.ef90y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "lolan4")
+DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
